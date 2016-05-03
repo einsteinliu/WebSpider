f764ff431545186084efbcd8324cdcfbd53f1431
einst <flybird1987@gmail.com>
2016-05-04 00:19:05 +0200

    done, can work now

--- WebSpider/WebSpider.py
+++ WebSpider/WebSpider.py
@@ -2,13 +2,50 @@ from bs4 import BeautifulSoup
 import urllib2,urllib
 import os
 import os.path
+import wget
+import sys
+import shutil
+
+saveToFolder = ''
+if(len(sys.argv) == 2):
+    saveToFolder = sys.argv[1]
+
+if(not os.path.exists(saveToFolder)):
+    os.mkdir(saveToFolder)
 
 if(not os.path.isfile("base.page")):
     print 'file base.page dosen\'t exit...'
+    exit(0)
 
 file = open("base.page","r")
-baseUrl = file.readline()
-if(len(baseUrl)<10):
+baseUrl = file.readline().replace('\n','')
+startUrl  = file.readline()
+if((len(startUrl)<10) or (len(baseUrl)<10)):
     print 'Settings in base.page invalid...'
 
+basePage = urllib2.urlopen(startUrl).read()
+soup = BeautifulSoup(basePage)
+content = soup.find_all('a',href=True)
 
+allDownloadedFiles = []
+for tag in content:
+    for attr in tag.attrs:
+        if (('title' in attr) and ('.jpg' in tag.attrs[attr])):
+            saveFile = unicode(tag.attrs['title'].replace('File:',''))
+            imageUrl = baseUrl + tag.attrs['href']
+            imagePage = urllib2.urlopen(imageUrl).read()
+            imageSoup = BeautifulSoup(imagePage)
+            bigImageUrl = ''
+            for imTag in imageSoup.find_all('a',href=True):
+                tagString = imTag.string
+                if(not tagString):
+                    continue
+                if((('x' in tagString) and ('pixels' in tagString)) or ('Original file' in tagString)):
+                    bigImageUrl = imTag.attrs['href']
+            if(len(bigImageUrl)>0):
+                currFileName = wget.download(bigImageUrl)
+                shutil.copyfile(currFileName.decode('utf8'),os.path.join(saveToFolder,saveFile))
+                allDownloadedFiles.append(currFileName)
+                    
+            
+    tagString = unicode(tag.string)
